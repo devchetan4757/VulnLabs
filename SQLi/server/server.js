a2c57d5ab0c394722bbcf86254b5392f7bd9e231
devchetan4757 <devchetanpatil@gmail.com>
2026-06-19 22:50:50 +0530

    Lab-2 SQLi server fix

--- SQLi/server/server.js
+++ SQLi/server/server.js
@@ -24,7 +24,7 @@ app.use("/api/reset", resetRoute);
 app.use(express.static(path.join(__dirname, "public")));
 
 // fallback route (VERY IMPORTANT)
-app.get("*", (req, res) => {
+app.use((req, res) => {
   res.sendFile(path.join(__dirname, "public", "index.html"));
 });
 
